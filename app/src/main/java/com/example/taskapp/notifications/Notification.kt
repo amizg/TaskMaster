0e431d76345519212d0c5ac06a9b93aa943430d1
CollinTift <collin.tift@gmail.com>
2022-04-13 23:49:47 -0400

    start of notifs

---
+++ app/src/main/java/com/example/taskapp/notifications/Notification.kt
@@ -0,0 +1,27 @@
+package com.example.taskapp.notifications
+
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.example.taskapp.R
+
+const val notificationID = 1
+const val channelID = "channel1"
+const val titleExtra = "titleExtra"
+const val messageExtra = "messageExtra"
+
+class Notification : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val notification = NotificationCompat.Builder(context, channelID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(intent.getStringExtra(titleExtra))
+            .setContentText(intent.getStringExtra(messageExtra))
+            .build()
+
+        val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.notify(notificationID, notification)
+    }
+
+}
